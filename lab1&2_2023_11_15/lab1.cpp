0018e4159018ec034c79eaca75f52153c3aec9c9
Coolllla <1320269132@qq.com>
2023-11-17 00:25:53 +0800

    the first commit

---
+++ lab1&2_2023_11_15/lab1.cpp
@@ -0,0 +1,72 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+#define p  3.14;
+int lab1_2() {
+
+	double r, h;
+	std::cin >> r >> h;
+	double v = r * r * h / 3 * p;
+	std::cout << v;
+	return 0;
+}
+
+int lab1_1() {
+	int k = 0;
+	int i = k + 1;
+	cout << i++ << endl;
+	//int i = 1;
+	cout << i++ << endl;
+	cout << "Welcome to C++" << endl;
+	return 0;
+}
+
+int lab1_3() { 
+	cout << "char length:" << sizeof(char) << endl;
+	cout << "int length:" << sizeof(int) << endl;
+	cout << "bool length:" << sizeof(bool) << endl;
+	cout << "float length:" << sizeof(float) << endl;
+	return 0;
+}
+
+int lab1_4() {
+	unsigned int testunint = 65534;//oxfffe
+	cout << "output in unsigned int  type:"<<testunint << endl;//<<oct;
+	cout << "output in char type:!" << static_cast<char>(testunint) << endl;
+	cout << "output in short type:" << static_cast<short>(testunint) << endl;//为什么结果为-2?:
+	cout << "output in int type:" << static_cast<int>(testunint) << endl;
+	cout << "output in double type:" << static_cast<double>(testunint) << endl;
+	cout << "output in double type:" << setprecision(4) << static_cast<double>(testunint) << endl;
+	cout << "output in hex unsigned int type:" << hex << testunint << endl; //16进制输出
+	cout << "output in oct unsigned int type:" << oct << testunint << endl; //8进制输出
+	system("pause");
+	cout << "以下是将实数转化为int" << endl;
+	double x = 13.456;
+	int y = int(x);
+	cout << "转换前"<<x << endl;
+	cout << "转换后"<<y << endl;
+	return 0;
+}
+
+int lab1_5() {
+	double h,t;
+	cin >> h;
+	t = (h - 32) / 1.8;
+	cout<< fixed<<setprecision(2)<<t;
+	return 0;
+}
+
+int main() {
+	int a;
+	cout << "输入数字1~5，对应1~5题" << endl;
+	cin >> a;
+	switch (a)
+	{
+	case 1: lab1_1(); break;
+	case 2:lab1_2(); break; 
+	case 3:lab1_3(); break;
+	case 4:lab1_4(); break;
+	case 5:lab1_5(); break;
+	}
+	return 0;
+}
---
+++ lab1&2_2023_11_15/lab2.cpp
@@ -0,0 +1,159 @@
+﻿#include<iostream>
+#include<string>
+#include"stdio.h"
+using namespace std;
+
+int lab2_1() {
+	char ch;
+	cin >> ch;
+	if ('a' <=ch && ch<='z') {
+		cout << char(ch - 'a' + 'A') << endl;
+	}
+	else cout << int(ch) + 1 << endl;
+	return 0;
+}
+
+int lab2_2() {
+	double x,y;
+	cin >> x;
+	if (0 < x && x < 1)
+		y = 3 - 2 * x;
+	else
+		if (1 <= x && x < 5)
+			y = 2.0 / 4 / x + 1;
+		else if (5 <= x && x < 10)
+			y = x * x;
+	if (0<x&&x<10)
+		cout << y << endl;
+	return 0;
+}
+
+int lab2_3() {
+	double x, y, z;
+	cin >> x >> y >> z;
+	//double m = max(x, y, z);
+	if (max(x,max(y,z)) < x + y + z - max(x,max(y,z))) {
+		cout << "周长是：" << x + y + z << endl;
+		if (x == y || x == z || y == z)
+			cout << "是等腰三角形" << endl;
+	}
+	else cout << "不是三角形" << endl;
+	return 0;
+}
+
+int lab2_4() {
+	double x, y;
+	char s;
+	cin >> x >> s >> y;
+	switch (s)
+	{
+	case '+':cout << x + y << endl; break;
+	case '-':cout << x - y << endl; break;
+	case '*':cout << x * y << endl; break;
+	case '/':cout << x / y << endl; break;
+	case '%':cout << int(x) % int(y) << endl; break;
+	default: {cout << "非法字符" << endl; break; }
+	}
+	return 0;
+}
+
+int lab2_5() {
+	char s1;
+	bool flag = true;
+	int a=0, b=0, c=0, d=0;
+	cin >> s1;
+	while (s1!='\n') {
+		if ('A' <= s1 && s1 <= 'Z' || 'a' <= s1 && s1 <= 'z')
+			a++;
+		else
+			if (' ' == s1)
+				b++;
+			else
+				if ('0' <= s1 && s1 <= '9')
+					c++;
+				else d++;
+		s1 = getchar();
+	}
+	cout << " 字母共有：" << a << "个" << endl;
+	cout << " 空格共有：" << b << "个" << endl;
+	cout << " 数字共有：" << c << "个" << endl;
+	cout << " 其他字符共有：" << d << "个" << endl;
+	return 0;
+}
+
+int lab2_6() {
+	int x, y,a,b,t;
+	cin >> x >> y;
+	a = max(x, y);
+	b = min(x, y);
+	t = a % b;
+	int i = a;
+	while (true) {
+		if (i % a == 0 && i % b == 0)
+			break;
+		else i++;
+	}
+	cout << "最小公倍数是：" << i << endl;
+	while (t != 0) {
+		a = b;
+		b = t;
+		t = a % b;
+	}
+	cout << "最大公因数是：" << b << endl;
+
+	return 0;
+}
+
+int lab2_7() {
+	char ch = '*';
+	for (int j = 1; j <= 5; j++) {
+		for (int i = 1; i <= j; i++)
+			cout << ch;
+		cout << "\n";
+	}
+	return 0;
+}
+
+int lab2_8() {
+	double x(0), a, x1(0);
+	cin >> a;
+	 x = a;
+	while (fabs(x1 - x) > 10e-5 && a>0) {
+		x1 = x;
+		x = 0.5 * (x1 + a / x1);
+	}
+	if (x > 0)
+		cout << x << endl;
+	else cout << "a是负数" << endl;
+	return 0;
+}
+
+int lab2_9() {
+	double sum=0;
+	int day = 1, a = 2,amount=2;
+	while (amount <= 100) {
+		sum += a * 0.8;
+		a = a * 2;
+		amount += a;
+		day++;
+	}
+	cout << "平均每天花了" << sum / (day-1) << "￥";
+	return 0;
+}
+
+int main() {
+	int a;
+	cin >> a;
+	switch (a)
+	{
+	case 1: lab2_1(); break;
+	case 2:lab2_2(); break;
+	case 3:lab2_3(); break;
+	case 4:lab2_4(); break;
+	case 5:lab2_5(); break;
+	case 6:lab2_6(); break;
+	case 7:lab2_7(); break;
+	case 8:lab2_8(); break;
+	case 9:lab2_9(); break;
+	}
+}
